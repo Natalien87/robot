84f2b25653c2b4093442598aba0b934340d363f8
Natasa <natkon87@gmail.com>
2022-02-26 19:06:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: '',
+        }
+    }
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> {
+         return response.json();
+      })
+      .then(users=> {
+         this.setState({robots: users})
+      });
+    }
+
+    OnSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if(this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        }else {
+            return (
+                <div className="tc">
+                    <h1>RobotFriends</h1>
+                    <SearchBox searchChange={this.OnSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots} />
+                    </Scroll>
+                    
+                </div>
+               
+            );
+        }
+
+    
 }
-
+}
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Boban',
+        email: 'boban.gmail.com',
+
+    },
+    {
+        id: 2,
+        name: 'Goran',
+        email: 'goran.gmail.com',
+        
+    },
+    {
+        id: 3,
+        name: 'Ivan',
+        email: 'ivan.gmail.com',
+        
+    },
+    {
+        id: 4,
+        name: 'Milan',
+        email: 'milan.gmail.com',
+        
+    }
+]
